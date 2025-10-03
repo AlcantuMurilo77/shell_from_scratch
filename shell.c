20a6c2d82e98f08396f9f74514b8bb87130a6dce
Murilo Alcantu <alcantumurilo@gmail.com>
2025-10-02 23:39:41 -0400

    Build one-use only shell

--- shell.c
+++ shell.c
@@ -1,8 +1,16 @@
 #include "shell.h"
 #include <stdio.h>
 
-int main(void){
+int main(int ac, char **av){
 
-    printf("Hello darling");
+    (void)ac; //why write (void) before?
+    int status;
+
+    if (fork() == 0){
+        execvp(av[1], av+1);
+    }
+
+    wait(&status);
+
     return EXIT_SUCCESS;
 }
