a0071be7655e6fd1690ac3b41edd95ea3f1b609b
Murilo Alcantu <alcantumurilo@gmail.com>
2025-10-03 00:31:31 -0400

    Add shell read line function and test EOF

--- shell.c
+++ shell.c
@@ -1,16 +1,46 @@
 #include "shell.h"
 #include <stdio.h>
 
+
+char *shell_read_line(void){
+    char *buf;
+    size_t bufsize; //what is size_t? 
+
+    buf = NULL;
+
+    if (getline(&buf, &bufsize, stdin) == -1){
+
+        buf = NULL;
+        if (feof(stdin)){ //why do we check for eof?
+            p(RED"[EOF]"RST); //Why do we define as red and after it use rst? is it to come back to original colours?
+        } else{
+            p(RED"Getline failed."RST);
+        }
+
+        p("%s", buf ? buf : "((null))");; //TODO
+    } //how does getline works?
+
+    return buf;
+} //why is the function a pointer?
+
+
 int main(int ac, char **av){
+    //Now we have to implement a REPL
+    // READ->EVALUATE->PRINT/EXECUTE->LOOP
 
-    (void)ac; //why write (void) before?
-    int status;
+    char *line;
 
-    if (fork() == 0){
-        execvp(av[1], av+1);
+    while(0xCE77){ //what are we doing here with physical memory addresses?? magic numbers? what?
+        // 1) get line
+        // 2) get tokens (we gonna use the function gettok but ideally we should use lexing->parsing to evaluate)
+        // 3) execute
+
+        line = shell_read_line(); //TODO: Implement this function
+
+        p("%s\n", line ? line : "((null))");p("%s\n", line);
+        pause();
+
     }
 
-    wait(&status);
-
-    return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
--- shell.h
+++ shell.h
@@ -6,4 +6,13 @@
 #include <unistd.h>
 
 
+#define Y "\033[1;33m"
+#define G "\033[1;32m"
+#define C "\033[1;36m"
+#define RED "\033[1;31m"
+#define RST "\033[0m"
+
+#define p(...) printf(__VA_ARGS__) //this defines a shortcut, now printf will be called with p
+
+
 #endif
